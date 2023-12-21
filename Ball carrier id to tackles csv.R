4af568c605335d3b347fe6d640916781ce9978ac
Charles Wells <gamesc@smcewells.com>
2023-12-21 14:40:10 -0500

    Yata w/ threshold

--- Ball carrier id to tackles csv.R
+++ Ball carrier id to tackles csv.R
@@ -1,9 +1,9 @@
 library(dplyr)
-plays <- read.csv("/Users/jonahlubin/Desktop/nfl-big-data-bowl-2024/plays.csv")
-tackles <- read.csv("/Users/jonahlubin/Desktop/nfl-big-data-bowl-2024/tackles.csv")
-tracking_w1 <- read.csv("/Users/jonahlubin/Desktop/nfl-big-data-bowl-2024/tracking_week_1.csv")
+plays <- read.csv("data/plays.csv")
+tackles <- read.csv("data/tackles.csv")
+tracking_w1 <- read.csv("data/tracking_week_1.csv")
 play_56 <- tracking_w1 %>%
-  filter(playId=="56", gameId=="2022090800")
+  filter(playId == "56", gameId == "2022090800")
 table(play_56$displayName)
 table(tracking_w1$event)
 
@@ -16,5 +16,5 @@ tackles$whole_id <- paste0(tackles$gameId, tackles$playId)
 
 tackles_with_bc_id <- merge(tackles, ball_carrier_id_info, by = ("whole_id"))
 
-write.csv(tackles_with_bc_id, "tackles_bc_id.csv")
+write.csv(tackles_with_bc_id, "data/tackles_bc_id1.csv")
 
--- yata.py
+++ yata.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     tackles = pd.read_csv("data/tackles_bc_id.csv")
-    print("read tackles")
+    print("Read tackles")
     weeks = []
 
     for week in range(1, 10):
         weeks.append(pd.read_csv(f"data/tracking_week_{week}.csv"))
-    print("read weeks")
+    print("Read weeks")
 
     tacklers = tackles.nflId.unique()
     missed_tackles = tackles.loc[tackles["pff_missedTackle"] == 1].whole_id
@@ -28,8 +28,8 @@ if __name__ == "__main__":
 
         dists = []
 
-        for week in weeks:
-            for row in player_tackles.itertuples():
+        for row in player_tackles.itertuples():
+            for week in weeks:
                 play = week.loc[week["gameId"] == row.gameId].loc[week["playId"] == row.playId]
                 if play.empty:
                     # wrong week
@@ -54,13 +54,14 @@ if __name__ == "__main__":
                 carrier_frame_cont = carrier_track.loc[carrier_track["frameId"] == contact_frame]
                 carrier_frame_tackle = carrier_track.loc[carrier_track["frameId"] == tackle_frame]
 
-                if carrier_frame_cont.empty or carrier_frame_tackle.empty:
+                if carrier_frame_cont.empty or carrier_frame_tackle.empty or tackle_frame - contact_frame > 50:
                     break
 
                 dist = math.sqrt((carrier_frame_cont["x"].iat[0] - carrier_frame_tackle["x"].iat[0]) ** 2 + (carrier_frame_cont["y"].iat[0] - carrier_frame_tackle["y"].iat[0]) ** 2)
                 dists.append(dist)
                 all_dists.append(dist)
                 all_times.append(tackle_frame - contact_frame)
+                break
         
         if len(dists) != 0:
             ids.append(tackler)
@@ -75,6 +76,6 @@ if __name__ == "__main__":
     plt.hist(all_dists, bins=range(1, 40))
     plt.show()
     plt.scatter(all_times, all_dists)
-    plt.xlabel("Contact to Down Time (Seconds)")
+    plt.xlabel("Contact to Down Time (Frames)")
     plt.ylabel("Contact to Down Distance (Yards)")
     plt.show()
