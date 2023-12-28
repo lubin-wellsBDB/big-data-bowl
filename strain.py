4f1f82c64226cf2480d5992b5629ed2cf30d4893
Charles Wells <gamesc@smcewells.com>
2023-12-28 14:00:57 -0500

    New Strain

--- strain.py
+++ strain.py
@@ -1,83 +1,81 @@
 import math
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    tackles = pd.read_csv("data/tackles_bc_id.csv")
-    print("read tackles")
     weeks = []
 
     for week in range(1, 10):
         weeks.append(pd.read_csv(f"data/tracking_week_{week}.csv"))
-    print("read weeks")
+    print("Read weeks")
 
-    tacklers = tackles.nflId.unique()
+    plays = pd.read_csv("data/plays.csv")
+    print("Read plays")
 
-    ids = []
-    maxes = []
-    means = []
-    occs = []
+    ids_to_data = {}
 
-    for tackler in tacklers:
-        print(tackler)
-        player_tackles = tackles.loc[tackles["nflId"] == tackler].loc[tackles["pff_missedTackle"] == 0]
-
-
-        # the following code is based off of Nguyen et al.'s STRAIN paper
-        # We did not use their code, but this is almost directly following
-        # their calculations - except we average strain by play as opposed to
-        # by frame - partially for ease, and partially because we felt that 
-        # made more sense.
-        averages = []
-
+    for row in plays.itertuples():
+        print(f"Play: {row.gameId}{row.playId}")
+        
         for week in weeks:
-            for row in player_tackles.itertuples():
-                play = week.loc[week["gameId"] == row.gameId].loc[week["playId"] == row.playId]
-                if play.empty:
-                    # wrong week
-                    continue
+            play = week.loc[week["gameId"] == row.gameId].loc[week["playId"] == row.playId]
+            if play.empty:
+                # wrong week
+                continue
 
-                tackler_track = play.loc[play["nflId"] == row.nflId]
-                carrier_track = play.loc[play["nflId"] == row.ballCarrierId]
+            carrier_track = play.loc[play["nflId"] == row.ballCarrierId]
 
-                success = True
+            if carrier_track.empty:
+                break # can't use this play
+
+            defense = play.loc[play["club"] != carrier_track["club"].iat[0]].loc[play["club"] != "football"]
+
+            for defender in defense.nflId.unique():
                 strains = []
+                success = True
 
-                # find tackler and carrier info in that frame
-                for tackler_frame in tackler_track.itertuples():
-                    if tackler_frame.frameId == 1:
+                defender_track = defense.loc[defense["nflId"] == defender]
+                for def_frame in defender_track.itertuples():
+                    if def_frame.frameId == 1:
                         continue
+                    
+                    carrier_frame = carrier_track.loc[carrier_track["frameId"] == def_frame.frameId]
+                    carrier_frame_prev = carrier_track.loc[carrier_track["frameId"] == def_frame.frameId - 1]
+                    def_frame_prev = defender_track.loc[defender_track["frameId"] == def_frame.frameId - 1]
 
-
-                    carrier_frame = carrier_track.loc[carrier_track["frameId"] == tackler_frame.frameId]
-                    carrier_frame_prev = carrier_track.loc[carrier_track["frameId"] == tackler_frame.frameId - 1]
-                    tackler_frame_prev = tackler_track.loc[tackler_track["frameId"] == tackler_frame.frameId - 1]
-
-                    if carrier_frame.empty or carrier_frame_prev.empty or tackler_frame_prev.empty:
+                    if carrier_frame.empty or carrier_frame_prev.empty or def_frame_prev.empty:
                         success = False
                         break
 
-                    curr_dist = math.sqrt((tackler_frame.x - carrier_frame["x"].iat[0]) ** 2 + (tackler_frame.y - carrier_frame["y"].iat[0]) ** 2)
+                    curr_dist = math.sqrt((def_frame.x - carrier_frame["x"].iat[0]) ** 2 + (def_frame.y - carrier_frame["y"].iat[0]) ** 2)
 
                     if curr_dist == 0:
                         # seems reasonable to call the play dead if the tackler is directly ontop of the ball carrier - divide by zero error otherwise
                         # keeping success as true, since I think this seems like a reasonable calculation in the case where the tackler reaches the carrier
                         break 
 
-                    prev_dist = math.sqrt((tackler_frame_prev["x"].iat[0] - carrier_frame_prev["x"].iat[0]) ** 2 + (tackler_frame_prev["y"].iat[0] - carrier_frame_prev["y"].iat[0]) ** 2)
+                    prev_dist = math.sqrt((def_frame_prev["x"].iat[0] - carrier_frame_prev["x"].iat[0]) ** 2 + (def_frame_prev["y"].iat[0] - carrier_frame_prev["y"].iat[0]) ** 2)
                     strains.append((-(curr_dist - prev_dist)/.1)/curr_dist)
                 
                 if success and len(strains) != 0:
-                    averages.append(sum(strains)/len(strains))
-        
-        if len(averages) != 0:
-            ids.append(tackler)
-            maxes.append(max(averages))
-            means.append(sum(averages)/len(averages))
-            occs.append(len(averages))
+                    if defender in ids_to_data:
+                        ids_to_data[defender].append(sum(strains)/len(strains))
+                    else:
+                        ids_to_data[defender] = [sum(strains)/len(strains)]
 
+    ids = []
+    maxes = []
+    means = []
+    occs = []
 
+    for player in ids_to_data:
+        ids.append(math.floor(player))
+        maxes.append(max(ids_to_data[player]))
+        means.append(sum(ids_to_data[player])/len(ids_to_data[player]))
+        occs.append(len(ids_to_data[player]))
 
     d = {"tacklerId": ids, "maxAvgStrain": maxes, "avgAvgStrain": means, "occurances":occs}
     df = pd.DataFrame(data=d)
-    df.to_csv("data/tackler_avg_strain.csv", index=False)
+    df.to_csv("data/strain.csv", index=False)
+
